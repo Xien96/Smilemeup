d8409455747dc9d00fd8b3412b9ac176662f9501
EslamMoh <eslammoh.ce@gmail.com>
2018-05-09 17:50:46 +0200

    fix current user issue

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,7 +4,7 @@ class CartController < ApplicationController
     @cart = session[:cart]
     @total = 0
     @cart.each do |ci|
-      price = Product.find_by(name: ci['product']).product_prices.find_by(:currency => user.currency).price
+      price = Product.find_by(name: ci['product']).product_prices.find_by(:currency => current_user.currency).price
       quantity = ci['quantity'].to_i
       @total += (price * quantity)
     end
