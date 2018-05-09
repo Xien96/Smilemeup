aa59f73d8b0cc5161046231ccc4aa7eaab826704
EslamMoh <eslammoh.ce@gmail.com>
2018-05-09 13:03:50 +0200

    Add currency to products prices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509004225) do
+ActiveRecord::Schema.define(version: 20180509110000) do
 
   create_table "pictures", force: :cascade do |t|
     t.string "pic_file_name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180509004225) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "currency", default: "usd"
     t.index ["product_id"], name: "index_products_prices_on_product_id"
   end
 
